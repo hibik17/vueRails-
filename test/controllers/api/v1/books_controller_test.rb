6e4636e14fee8118bf09f7eb4c9fc42ddb087869
hibik17 <hbtatsu@gmail.com>
2022-11-22 14:26:13 +0900

    add books controller under api/v1

---
+++ test/controllers/api/v1/books_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class Api::V1::BooksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_books_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_v1_books_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get api_v1_books_edit_url
+    assert_response :success
+  end
+end
