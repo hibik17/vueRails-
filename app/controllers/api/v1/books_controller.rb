ddf63183d0ab9ac000553c7cb4a50482392ad9ea
hibik17 <hbtatsu@gmail.com>
2022-11-22 14:43:10 +0900

    api/v1/books/index 配下でbooksのデータの返却を完了

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,10 +1,13 @@
 class Api::V1::BooksController < ApplicationController
   def index
+    render json: Book.all
   end
 
   def show
+    render :json => Book.find(params[:id]), status: 'sucess'
   end
 
   def edit
+    render :json => Book.find(params[:id]), status: 'sucess'
   end
 end
