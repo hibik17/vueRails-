d96732326b64ddd0ce90a3af2ffc44f809ff9263
hibik17 <hbtatsu@gmail.com>
2022-12-08 16:35:31 +0900

    add form data

--- app/javascript/components/pages/books/Show.vue
+++ app/javascript/components/pages/books/Show.vue
@@ -13,19 +13,26 @@
 
 <script setup>
 import axios from "axios";
-import { onMounted, reactive } from "vue";
+import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
 import LeftContent from "../../organisms/LeftContent.vue";
 import RightContent from "../../organisms/RightContent.vue";
 
 const router = useRoute();
-const book = reactive([]);
+const book = ref({
+  title: "",
+  body: "",
+});
 
 onMounted(() => {
   axios
     .get(`http://localhost:3000/api/v1/books/${router.params.id}`)
     .then((res) => {
-      const book = reactive([res.data]);
+      console.log(res.data);
+      book.value = {
+        title: res.data.title,
+        body: res.data.body,
+      };
     })
     .catch((err) => console.log(err))
     .finally(() => console.log("data fetching done ..."));
